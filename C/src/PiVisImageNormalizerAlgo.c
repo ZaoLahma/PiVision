ee23bbd4a5116978ad5beba036e974790c52f85a
ZaoLahma <thyberg.jan@gmail.com>
2018-02-02 20:00:42 +0100

    Playing around with image filters

--- C/src/PiVisImageNormalizerAlgo.c
+++ C/src/PiVisImageNormalizerAlgo.c
@@ -3,13 +3,18 @@
 #include "PiVisConstants.h"
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #define U8_MAX (0xFFu)
+#define IMAGE_FILTER_HIGH (240)
+#define IMAGE_FILTER_LOW  (10)
 
 static char grayscaleImage[(GRAYSCALE_IMAGE_SIZE)];
 static char normalizedImage[(GRAYSCALE_IMAGE_SIZE)];
-static unsigned int maxIntensity = 0u;
-static unsigned int minIntensity = 255u;
+static int maxIntensity = 0u;
+static int minIntensity = 255u;
+static int averageIntensity = 0u;
+static int standardDeviation = 0u;
 
 static void createGrayscaleImage(char* colorImage);
 static void normalizeImage(void);
@@ -30,17 +35,8 @@ static void createGrayscaleImage(char* colorImage)
     {
       colorIndex = 0u;
       colorIntensity = (colorIntensity / 3u);
+
       grayscaleImage[grayscalePixelIndex] = colorIntensity;
-
-      if(colorIntensity > maxIntensity)
-      {
-        maxIntensity = colorIntensity;
-      }
-
-      if(colorIntensity < minIntensity)
-      {
-        minIntensity = colorIntensity;
-      }
 
       colorIntensity = 0u;
       grayscalePixelIndex += 1u;
@@ -50,11 +46,71 @@ static void createGrayscaleImage(char* colorImage)
 
 static void normalizeImage(void)
 {
-  unsigned int normalizingFactor = (255 / (maxIntensity - minIntensity));
   unsigned int bufIndex = 0u;
+  int colorIntensity = 0u;
   for(; bufIndex < (GRAYSCALE_IMAGE_SIZE); ++bufIndex)
   {
-    normalizedImage[bufIndex] = (grayscaleImage[bufIndex] - minIntensity) * normalizingFactor;
+    colorIntensity = grayscaleImage[bufIndex];
+
+    if(colorIntensity > IMAGE_FILTER_HIGH)
+    {
+      colorIntensity = IMAGE_FILTER_HIGH;
+    }
+    else if(colorIntensity < IMAGE_FILTER_LOW)
+    {
+      colorIntensity = IMAGE_FILTER_LOW;
+    }
+
+    averageIntensity += colorIntensity;
+
+    normalizedImage[bufIndex] = colorIntensity;
+  }
+
+  averageIntensity = averageIntensity / (GRAYSCALE_IMAGE_SIZE);
+  unsigned int imageVariance = 0u;
+
+  bufIndex = 0u;
+  for(; bufIndex < (GRAYSCALE_IMAGE_SIZE); ++bufIndex)
+  {
+    imageVariance += (normalizedImage[bufIndex] - averageIntensity) * (grayscaleImage[bufIndex] - averageIntensity);
+  }
+  imageVariance = imageVariance / (GRAYSCALE_IMAGE_SIZE);
+
+  standardDeviation = sqrt(imageVariance);
+
+  (void) printf("averageIntensity: %u, standardDeviation: %u\n", averageIntensity, standardDeviation);
+
+  bufIndex = 0u;
+  for(; bufIndex < (GRAYSCALE_IMAGE_SIZE); ++bufIndex)
+  {
+    colorIntensity = normalizedImage[bufIndex];
+
+    if(normalizedImage[bufIndex] > averageIntensity)
+    {
+      colorIntensity = (normalizedImage[bufIndex] - averageIntensity) / standardDeviation;
+    }
+
+    if(colorIntensity > maxIntensity)
+    {
+      maxIntensity = colorIntensity;
+    }
+
+    if(colorIntensity < minIntensity)
+    {
+      minIntensity = colorIntensity;
+    }
+
+    normalizedImage[bufIndex] = colorIntensity;
+  }
+
+  unsigned int normalizingFactor = (255 / (maxIntensity - minIntensity));
+  bufIndex = 0u;
+  for(; bufIndex < (GRAYSCALE_IMAGE_SIZE); ++bufIndex)
+  {
+    if(normalizedImage[bufIndex] > minIntensity)
+    {
+      normalizedImage[bufIndex] = (normalizedImage[bufIndex] - minIntensity) * normalizingFactor;
+    }
   }
 }
 
