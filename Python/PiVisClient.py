357fcc33c02c8b0c503f35b12edea9f117a9f8c6
ZaoLahma <thyberg.jan@gmail.com>
2018-05-02 19:28:38 +0200

    Fixed new weird bug in python gui...

--- Python/PiVisClient.py
+++ Python/PiVisClient.py
@@ -9,7 +9,7 @@ class PiVisClient:
         self.serviceNo = -1
         self.portNo = portNo
         self.serviceNo = PiVisConstants.DISCOVER_IMAGE_DATA_SERVICE
-        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.serviceDiscoverySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.serviceDiscoverySocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
         self.stateIndex = 0
@@ -18,24 +18,25 @@ class PiVisClient:
         self.imageSize = imageSize
         self.data = []
         scheduler.registerRunnable(self.run)
-    
+
     def findService(self):
         data = bytearray()
+        address = bytearray()
         data.extend(map(ord, PiVisConstants.SERVICE_DISCOVER_REQUEST_HEADER + str(self.portNo)))
         self.serviceDiscoverySocket.sendto(data,  ("224.1.1.1", self.serviceNo))
         self.serviceDiscoverySocket.settimeout(1)
         try:
-            data = self.serviceDiscoverySocket.recv(13)
+            address = self.serviceDiscoverySocket.recv(13)
         except socket.timeout:
             pass
         except:
             raise
         else:
             self.serviceDiscoverySocket.close()
-            self.piVisServerAddress = str(data, 'utf-8')
-            print("PiVisServer service found at " + str(self.piVisServerAddress))
+            self.piVisServerAddress = str(address, 'utf-8').split('\x00')[0]
+            print("PiVisServer service found at " + self.piVisServerAddress)
             self.stateIndex += 1
-    
+
     def connect(self):
         try:
             print("Attempting to connect to " + self.piVisServerAddress + " " + str(self.portNo))
@@ -45,14 +46,14 @@ class PiVisClient:
         else:
             print("Connection established to PiVision server")
             self.stateIndex = self.stateIndex + 1
-            
+
     def connected(self):
         self.receive()
-        
+
     def getData(self):
         retVal = self.data
         return retVal
-    
+
     def __receiveInternal(self, numBytes):
         data = []
         while len(data) < numBytes:
@@ -63,8 +64,8 @@ class PiVisClient:
                 data += packet
             except socket.timeout:
                 pass
-        return data       
-        
+        return data
+
     def receive(self):
         headerSize = 9 #1 byte image type, 4 bytes image size, 2 bytes xSize, 2 bytes ySize
 
@@ -74,16 +75,16 @@ class PiVisClient:
         imageSize = struct.unpack("<L", imageSize)[0]
 
         imageType = header[0:1]
-        
+
         receiveBuf = self.__receiveInternal(imageSize)
-        
+
         tmpBuf = []
         tmpBuf += imageType
-        tmpBuf += receiveBuf    
+        tmpBuf += receiveBuf
         self.data = tmpBuf
-        
+
     def send(self, data):
         self.serverSocket.sendall(data)
-    
+
     def run(self):
         self.states[self.stateIndex]()
