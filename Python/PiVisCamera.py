826c96f017f7938f433abf45623ef11ab0a2b418
ZaoLahma <thyberg.jan@gmail.com>
2018-05-10 15:46:33 +0200

    Started adding meta data to camera image

--- Python/PiVisCamera.py
+++ Python/PiVisCamera.py
@@ -9,19 +9,27 @@ import io
 import sys
 
 class PiVisCamera:
-    def __init__(self, scheduler, server, resolution):
+    def __init__(self, scheduler, server, resolution, appendRes=False):
         self.server = server
         self.camera = picamera.PiCamera()
         self.camera.rotation = 180
         self.resolution = resolution
         self.camera.resolution = self.resolution
+        self.appendRes = appendRes
         time.sleep(2)
         scheduler.registerRunnable(self.run)
 
     def run(self):
         currImage = io.BytesIO()
         self.camera.capture(currImage, 'rgb', use_video_port=True)
-        self.server.send(bytearray(currImage.getvalue()))
+        imageData = bytearray()
+        if self.appendRes:
+            xRes = self.resolution[0]
+            yRes = self.resolution[1]
+            imageData.append(xRes.to_bytes(2, byteorder='little'))
+            imageData.append(yRes.to_bytes(2, byteorder='little'))
+        imageData.append(currImage.getvalue())
+        self.server.send(imageData)
 
 if __name__ == "__main__":
     print("PiVisCamera starting")
