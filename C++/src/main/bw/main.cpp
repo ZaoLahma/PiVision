99f8f7c1ab99173689d13b40454969836aee968e
ZaoLahma <thyberg.jan@gmail.com>
2018-05-31 22:39:01 +0200

    Made it possible to split the application... First step of distributed services

---
+++ C++/src/main/bw/main.cpp
@@ -0,0 +1,30 @@
+#include "jobdispatcher.h"
+#include "pivision_servicehandler.h"
+#include "pivision_threadmodel.h"
+#include "pivision_framecoord.h"
+#include "pivision_grayscaleimage.h"
+#include "pivision_ethterm.h"
+#include "pivision_events.h"
+#include <memory>
+#include <iostream>
+
+int main(void)
+{
+  JobDispatcher::GetApi()->Log("PiVision start");
+
+  JobDispatcher::GetApi()->AddExecGroup(EVENT_EXEC_GROUP_ID, 4u);
+  JobDispatcher::GetApi()->AddExecGroup(PIVISION_FRAMECOORD_THREAD_ID, 0u);
+  JobDispatcher::GetApi()->AddExecGroup(PIVISION_SERVICE_DISCOVERY_THREAD_ID, 5u);
+  JobDispatcher::GetApi()->AddExecGroup(PIVISION_CONNECTIONS_THREAD_ID, 10u);
+  JobDispatcher::GetApi()->AddExecGroup(PIVISION_SERVICE_LISTENER_THREAD_ID, 10u);
+
+  PiVisionEthTerm ethTerm;
+  PiVisionServiceHandler services;
+  PiVisionGrayscaleImage grayScale;
+
+  JobDispatcher::GetApi()->WaitForExecutionFinished();
+
+  JobDispatcher::DropInstance();
+
+  return 0;
+}
